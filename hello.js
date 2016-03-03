28021f0ec76626e18662b0fa46e449b7ba0c9bd4
Tonny Adhi Sabastian <tonny.adhi@ui.ac.id>
2016-03-03 10:36:17 +0700

    Add something on hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
+  res.end('Hello GitHub!\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
